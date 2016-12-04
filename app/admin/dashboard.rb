9d211ffb3c92b58ecc30d0c4abd1d49688114abd
Gal Bracha <galbra@gmail.com>
2016-12-04 11:48:49 +0200

    Updated admin stats

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,43 +3,18 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    default_coins = 10
 
     columns do
-        columns do
-            panel "Users" do
-            end
+      column do
+        panel "Info" do
+          para User.count.to_s + " registered users"
+          para (User.count * default_coins).to_s + " Total available coins for all users"
+          para Grant.sum(:amount).to_s + " coins were distributed"
+          para (Grant.sum(:amount) * Rails.application.config.coin_rate).to_s + " amount of money distributed"
+          para "Coins distribution " + Grant.group(:amount).count.to_s
         end
-        column do
-            panel "Info" do
-                para "Welcome to ActiveAdmin!"
-            end
-        end
+      end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
