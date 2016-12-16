377a00d8eb761a07d3772ee6d560168e2cd23db1
eyal <eyal.regular@gmail.com>
2016-12-16 18:21:19 +0200

    filter if a dream is active

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216094343) do
+ActiveRecord::Schema.define(version: 20161216151529) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20161216094343) do
     t.string   "safetybag_firstMemberEmail",  limit: 64
     t.string   "safetybag_secondMemberName",  limit: 64
     t.string   "safetybag_secondMemberEmail", limit: 64
+    t.boolean  "active",                                   default: true
   end
 
   add_index "camps", ["user_id"], name: "index_camps_on_user_id"
