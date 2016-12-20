2401b40dc664077c8db50cd15b6035ea60e119bb
Gal Bracha <galbra@gmail.com>
2016-12-20 23:59:27 +0200

    Updated schema file to latest file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219211858) do
+ActiveRecord::Schema.define(version: 20161219070103) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -92,7 +92,10 @@ ActiveRecord::Schema.define(version: 20161219211858) do
     t.string   "safetybag_firstMemberEmail",                             limit: 64
     t.string   "safetybag_secondMemberName",                             limit: 64
     t.string   "safetybag_secondMemberEmail",                            limit: 64
+    t.string   "about_the_artist",                                       limit: 1024
+    t.string   "website",                                                limit: 512
     t.boolean  "active",                                                              default: true
+    t.boolean  "is_public",                                                           default: false, null: false
     t.string   "dreamprop_philosophy",                                   limit: 4096
     t.string   "dreamprop_inspiration",                                  limit: 4096
     t.string   "dreamprop_interactivity_audience_participation",         limit: 4096
@@ -109,9 +112,6 @@ ActiveRecord::Schema.define(version: 20161219211858) do
     t.boolean  "dreamprop_community_is_interested_in_publicity",                      default: false, null: false
     t.boolean  "dreamprop_theme_is_annual",                                           default: false, null: false
     t.string   "dreamprop_theme_annual_desc",                            limit: 4096
-    t.string   "about_the_artist",                                       limit: 1024
-    t.string   "website",                                                limit: 512
-    t.boolean  "is_public",                                                           default: false, null: false
   end
 
   add_index "camps", ["user_id"], name: "index_camps_on_user_id"
