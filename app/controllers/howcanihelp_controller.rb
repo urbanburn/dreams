8f8e5fc22084b02f58ea38c7ba5d96c56421a27b
Gal Bracha <galbra@gmail.com>
2017-01-17 12:15:15 +0200

    Removed unused code

--- app/controllers/howcanihelp_controller.rb
+++ app/controllers/howcanihelp_controller.rb
@@ -21,9 +21,4 @@ class HowcanihelpController < ApplicationController
     end
   end
 
-  private
-  def valid_page?
-    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
-  end
-
 end
